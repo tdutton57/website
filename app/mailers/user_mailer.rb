dda4ecb54597025f081920df726cf2a6c7464c0d
Tiffani Dutton <tiffani.dutton@gmail.com>
2013-09-10 17:14:44 -0500

    adding website changes for resume handling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   def registration_confirmation(user) 
   	@user = user
-  	attachments["Resume.pdf"] = File.read("#{Rails.root}/public/files/Resume.pdf")  
-    mail(:to => user.email, :subject => "Resume", :from => "tiffani.dutton@gmail.com")  
+  	attachments["Dutton Tiffani Resume.pdf"] = File.read("#{Rails.root}/public/files/Dutton\ Tiffani\ Resume.pdf")  
+    mail(:to => user.email, :subject => "Tiffani Dutton Resume", :from => "tiffani.dutton@gmail.com")  
   end
 end
